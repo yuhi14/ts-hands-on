275e8c32d34b855476f4443e793a58e692c5a3f5
永田悠飛 <yuhi14skywalker@gmail.com>
2020-03-18 17:51:48 +0900

    study never

---
+++ src/never.ts
@@ -0,0 +1,20 @@
+export { };
+
+
+function error(message: string): never {
+    throw new Error(message);
+}
+
+
+try {
+    let result = error("エラーがおきました！");
+    console.log(result);
+} catch (error) {
+    console.log({ error });
+}
+
+let foo: void = undefined;
+// let bar: never = undefined;
+
+let bar: never = error("only me!");
+
