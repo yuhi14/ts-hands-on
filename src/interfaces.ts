247e5373a3ee75650f7b8082bd8cc7dbdda60e57
永田悠飛 <yuhi14skywalker@gmail.com>
2020-03-18 18:22:03 +0900

    study interface

---
+++ src/interfaces.ts
@@ -0,0 +1,16 @@
+export { };
+
+type ObjectType = {
+    name: string;
+    age: number;
+}
+
+interface ObjectInterface {
+    name: string;
+    age: number;
+}
+
+let object: ObjectInterface = {
+    name: "yuhi",
+    age: 27
+}
