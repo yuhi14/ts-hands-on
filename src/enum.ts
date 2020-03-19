a09abd97c9f84422adbd8e0554ab70d8e143de3e
永田悠飛 <yuhi14skywalker@gmail.com>
2020-03-19 15:06:02 +0900

    study enum

---
+++ src/enum.ts
@@ -0,0 +1,28 @@
+export { };
+
+enum Months {
+    January = 1,
+    February,
+    March,
+    April,
+    May,
+    Jun,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+}
+
+console.log(Months.January);
+
+enum Colors {
+    RED = '#ff0000',
+    WHITE = '#ffffff',
+    GREEN = '#008000',
+    BLUE = '0000ff',
+    BLACE = '000000'
+}
+let green = Colors.GREEN
+console.log({ green });
